c0f07c5d44f79c8e1715f1cda644c1261dd7147a
imekachi <imekachi@gmail.com>
2017-08-15 18:11:30 +0700

    - minor text change on AutoShareBox

--- src/components/AutoShareBox.js
+++ src/components/AutoShareBox.js
@@ -5,7 +5,7 @@ const AutoShareBox = (props) => (
   <div id={props.id} className={combineClassNames('remark-box', props.className || '')}>
 
     {props.forceUnchecked ? (
-      '*ยังไม่แชร์ผลลัพธ์'
+      '*ยังไม่แชร์ผลลัพธ์ไปยัง Facebook'
     ) : (
       <div className="autoshare-wrapper">
         <input type="checkbox" className="formcheckbox sharecheckbox" id={props.id}/>
