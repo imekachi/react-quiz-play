132c19fd5f8af23c9e88519226bba06851895d1a
imekachi <imekachi@gmail.com>
2017-09-14 17:35:21 +0700

    - fix invalid description

--- src/util/number.js
+++ src/util/number.js
@@ -11,9 +11,9 @@ export const isNumber = data => (!isNaN(parseFloat(data))) && isFinite(data)
  * stripping all non numeric character but reserve negativity number and decimal precisions
  * and returns a number ( Integer by default, but you can disable rounding, it will return float )
  *
- * getNumber('-1,234.43')           // -1234
- * getNumber('-12.54')              // -13
- * getNumber('1,234.445', false)    // 1234.445
+ * getNumber('-1,234.435')      // -1234.435
+ * getNumber('-12.54')          // -13
+ * getNumber('1,234.445', true) // 1234
  *
  * @param   {string|number} data        input string or number
  * @param   {boolean} [isRound = false] option if you want the number rounded or raw float
